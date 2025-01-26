fee65208dbb6ffc64cd22486897725c721307e8a
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-01-26 14:37:34 +0530

    name showing

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                     </div>
                     {!!user && (
                     <div>
-                        {user.name}
+                        {user.firstName}
                     </div>
                 )}
                 </Link>
--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -5,13 +5,17 @@ export const UserContext = createContext();
 
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
-    useEffect(()=>{
-  if(!user){
-    axios.get('/profile').then(({data}) => {
-      setUser(data);
-    })
-  }
-    },[])
+    useEffect(() => {
+      if (!user) {
+          axios.get('/profile', { withCredentials: true })
+              .then(({ data }) => {
+                  setUser(data);
+              })
+              .catch((error) => {
+                  console.error('Error fetching profile:', error);
+              });
+      }
+  }, [user]);
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
